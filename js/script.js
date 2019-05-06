b3c33733b607e0e40d635475bee33f904f94f622
njorogebrian <briannjoroge2000@gmail.com>
2019-05-06 10:52:37 +0300

    add hover effect

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
-$(document).ready(function(){
-  $("#one").click(function(){
+$(document).ready(function() {
+  $("#one").click(function() {
     $(".showing1").toggle();
   });
-  $("#two").click(function(){
+  $("#two").click(function() {
     $(".showing2").toggle();
   });
-  $("#three").click(function(){
+  $("#three").click(function() {
     $(".showing3").toggle();
   });
 
 
-  $("button").click(function(event){
+  $("button").click(function(event) {
     event.preventDefault();
     var user = document.getElementById('input1').value;
     alert(user + " we have received your message. Thank you for reaching out to us.");
   });
+  $(".para").hover(function() {
+      $(this).animate({
+        opacity: "1"
+      });
+    },
+    function() {
+      $this.animate({
+        opacity: 0
+      });
+    });
 });
